7b9fec2f88726f203effa5919ef994921124d902
Ecodrin <matvfo@ya.ru>
2026-04-08 13:50:54 +0300

    fix lab

--- scripts/run_etl.sh
+++ scripts/run_etl.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 source ./scripts/to_dwh.sh
 source ./scripts/to_clickhouse.sh
 source ./scripts/to_mongo.sh
-
 echo "success"
--- scripts/to_dwh.sh
+++ scripts/to_dwh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 docker exec spark /opt/spark/bin/spark-submit --master local[*] \
         --jars /opt/etl/drivers/postgresql-42.7.10.jar \
         /opt/etl/scripts/to_dwh.py
-
 echo "to dwh succes"
